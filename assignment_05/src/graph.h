c6915f7278c28e88caa8c0667b8068deec71049a
Anandu B Ajith <reach2anandu@gmail.com>
2022-02-21 19:52:39 +0530

    progress

---
+++ assignment_05/src/graph.h
@@ -0,0 +1,17 @@
+typedef struct _node {
+
+} Node;
+
+typedef struct _graph {
+
+} Graph;
+
+
+
+Graph make_graph(int, int);
+void add_edge();
+void print_graph(Graph);
+
+// I'm holding graph in Nodes[N] list
+// each node will have
+
---
+++ assignment_05/src/lsr.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () {
+    printf("Link State Routing\n");
+
+    return 0;
+}
