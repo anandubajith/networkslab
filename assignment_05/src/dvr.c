548bf9a034911b3308e170f4431c0974d3c2da9e
Anandu B Ajith <reach2anandu@gmail.com>
2022-02-22 23:16:36 +0530

    split implementations

--- assignment_05/src/dvr.c
+++ assignment_05/src/dvr.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
-#include<limits.h>
-#include "graph.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+typedef struct _edge {
+    int from;
+    int to;
+    int cost;
+} Edge;
+
+typedef struct _graph {
+    Edge *edges;
+    int num_nodes;
+    int num_edges;
+} Graph;
+
+
+Graph* make_graph(int node_count, int edge_count) {
+    Graph* g = malloc(sizeof(Graph));
+    g->num_nodes = node_count;
+    g->edges = malloc(sizeof(Edge) * edge_count*2);
+    g->num_edges = 0;
+    return g;
+}
+
+
+void add_edge(Graph* g, int from, int to, int cost) {
+
+    assert(from >= 0);
+    assert(to >= 0);
+
+    g->edges[g->num_edges].cost = cost;
+    g->edges[g->num_edges].from = from;
+    g->edges[g->num_edges].to = to;
+    g->num_edges++;
+}
+
+void print_graph(Graph *g) {
+    printf("num_nodes = %d, num_edges = %d \n", g->num_nodes, g->num_edges);
+    for (int i = 0; i < g->num_edges; i++) {
+        printf("Edge: %d %d %d\n", g->edges[i].from, g->edges[i].to, g->edges[i].cost);
+    }
+}
 
 void bellman_ford(Graph *g, int start){
     int dist[g->num_nodes];
@@ -62,7 +104,18 @@ void bellman_ford(Graph *g, int start){
 int main ()
 {
     printf("\nDistance Vector Routing (Bellman Ford)\n");
-    Graph* g= input_graph();
+    int num_nodes, num_edges;
+    scanf("%d %d", &num_nodes, &num_edges);
+
+    Graph *g = make_graph(num_nodes, num_edges);
+
+    int from, to, cost;
+    for ( int i = 0; i < num_edges; i++) {
+        scanf("%d %d %d", &from, &to, &cost);
+        add_edge(g, from-1, to-1, cost);
+        // Graphs are undirected
+        add_edge(g, to-1, from-1, cost);
+    }
 
     for ( int i = 0; i < g-> num_nodes; i++) {
         bellman_ford(g, i);
--- assignment_05/src/lsr.c
+++ assignment_05/src/lsr.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-#include "graph.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+typedef struct _edge {
+    int from;
+    int to;
+    int cost;
+} Edge;
+
+typedef struct _graph {
+    Edge *edges;
+    int num_nodes;
+    int num_edges;
+} Graph;
+
+Graph* make_graph(int node_count, int edge_count) {
+    return NULL;
+}
+
+void add_edge(Graph *g, int from, int to, int cost) {
+
+}
+
+void print_graph(Graph *g) {
+
+}
 
 // need to implement priorityQ
 
@@ -19,11 +46,18 @@ void dijkstra(Graph *g, int start) {
 
 int main () {
     printf("Link State Routing => Dijkstra\n");
-    Graph *g = input_graph();
-    /*
-     * Have global knowledge
-     * Run Dijkstra
-     */
+    int num_nodes, num_edges;
+    scanf("%d %d", &num_nodes, &num_edges);
+
+    Graph *g = make_graph(num_nodes, num_edges);
+
+    int from, to, cost;
+    for ( int i = 0; i < num_edges; i++) {
+        scanf("%d %d %d", &from, &to, &cost);
+        add_edge(g, from-1, to-1, cost);
+        // Graphs are undirected
+        add_edge(g, to-1, from-1, cost);
+    }
 
     for ( int i = 0; i < g-> num_nodes; i++) {
         dijkstra(g, i);
