778ba27ff0c46c52e66851db29a03b4ecfafa70f
Anandu B Ajith <reach2anandu@gmail.com>
2022-03-09 17:23:25 +0530

    ignore

---
+++ assignment_06/exec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+gcc -Wall -Wextra -g FTAPClient/FTAPClient.c -o FTAPClient/FTAPClient
+gcc -Wall -Wextra -g FTAPServer/FTAPServer.c -o FTAPServer/FTAPServer
+
+
+tmux new-session \; \
+    send-keys './FTAPServer/FTAPServer' C-m \; \
+    split-window -h \; \
+    send-keys './FTAPClient/FTAPClient' C-m \;
+
