c664e9a5b30731f7bf53d8ce0bbf5159d299c367
Anandu B Ajith <reach2anandu@gmail.com>
2022-03-15 21:51:14 +0530

    u

--- .tests/smtp/mailclient.c
+++ .tests/smtp/mailclient.c
@@ -1,3 +1,47 @@
 /*
  * THIS IS THE MAIL CLIENT
  */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    if ( argc < 2 ) {
+        printf("./smtpmail PORT\n");
+        return 1;
+    }
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(argv[1]));
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    /* int r = connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)); */
+    /* if (r == -1) { */
+    /*     printf("Error: Failed to connect()"); */
+    /*     return 0; */
+    /* } */
+
+    // ask for username
+    printf("username: ");
+    fflush(stdout);
+    printf("\n");
+    // ask for password
+    printf("password: ");
+    fflush(stdout);
+    printf("\n");
+
+    printf("Welcome %s\n", "asdf");
+    printf("1. Send Mail\n2. Quit\n");
+
+
+    return 0;
+}
--- .tests/smtp/smtpmail.c
+++ .tests/smtp/smtpmail.c
@@ -2,13 +2,16 @@
  * THIS IS THE SMTP MAIL SERVER
  */
 #include <stdio.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
+#define BACKLOG 5
 #define MAX_SIZE 100
 
 typedef struct _user {
@@ -126,24 +129,53 @@ int load_usersfile() {
         strcpy(password, line+i+1);
         password[strlen(password)-1] = '\0';
         add_user(username, password);
-        /* printf("%s:%s\n", username, password); */
+        /* printf("'%s':'%s'\n", username, password); */
     }
-    create_folders();
     return 0;
 }
-
+void handle_client(int socket) {
+    printf("Handling client with socket %d\n", socket);
+    // do SMTP Stuff
+}
 
 int main (int argc, char *argv[])
 {
     if ( argc < 2 ) {
         printf("./smtpmail PORT\n");
-        return 0;
+        return 1;
     }
+
     load_usersfile();
+    create_folders();
 
 
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if ( setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+        printf("setsockopt() failed");
 
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(argv[1]));
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
+    int success = bind(server_sock, (struct sockaddr*)&server_address, sizeof(server_address));
+    if ( success != 0) {
+        printf("bind() failed");
+        return 1;
+    }
+    listen(server_sock, BACKLOG);
+    printf("Listening on port %d\n", atoi(argv[1]));
+
+    while(1) {
+        int client_sock = accept(server_sock, NULL, NULL);
+        if ( client_sock == -1 ) continue;
+        if ( !fork() ) {
+            close(server_sock);
+            handle_client(client_sock);
+        }
+        close(client_sock);
+    }
+    close(server_sock);
 
     return 0;
 }
