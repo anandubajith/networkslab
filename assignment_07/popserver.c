f696cf753157959971c034322d368bfbfef0948b
Anandu B Ajith <reach2anandu@gmail.com>
2022-03-21 09:12:07 +0530

    u

--- assignment_07/popserver.c
+++ assignment_07/popserver.c
@@ -1,7 +1,3 @@
-/*
- * THIS IS THE POPSERVER to read email
- */
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -9,6 +5,41 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define MAX_SIZE 100
+#define BUF_SIZE 512
+
+typedef struct _mail {
+    char *from;
+    char *to;
+    char *subject;
+    char *received_time;
+    char *body;
+} Mail;
+
+
+void list_messages(char *username) {
+    char *path = malloc(sizeof(char) * MAX_SIZE * 2);
+    sprintf(path, "./%s/mymailbox", username);
+    FILE *fp = fopen(path, "r");
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+
+    // three states
+    // - mail start
+    // - headers
+    // - body
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        /* printf(line); */
+    }
+
+    if (line) free(line);
+
+}
 
 int main (int argc, char *argv[])
 {
@@ -24,7 +55,9 @@ int main (int argc, char *argv[])
     server_address.sin_port = htons(atoi(argv[1]));
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    printf("Pop serer");
+    printf("Pop server\n");
+
+    list_messages("anandu");
 
 
     return 0;
