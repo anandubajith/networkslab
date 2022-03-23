a9475f4d8ea45fadd047d8b21b3df985654fa045
Anandu B Ajith <reach2anandu@gmail.com>
2022-03-23 21:34:26 +0530

    exec file

---
+++ assignment_07/exec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gcc -Wall -Wextra -g mailclient.c -o mailclient
+gcc -Wall -Wextra -g popserver.c -o popserver
+gcc -Wall -Wextra -g smtpmail.c -o smtpmail
+
+
+tmux new-session \; \
+    send-keys './smtpmail 2525' C-m \; \
+    split-window -h \; \
+    send-keys './popserver 1110' C-m \; \
+    split-window -h \; \
+    send-keys './mailclient 2525 1110' C-m \; \
+
+
