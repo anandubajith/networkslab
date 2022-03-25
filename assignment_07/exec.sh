17393f600e1677bbe57cf279515689e2050c0333
Anandu B Ajith <reach2anandu@gmail.com>
2022-03-25 21:13:38 +0530

    improved cli

--- assignment_07/exec.sh
+++ assignment_07/exec.sh
@@ -2,17 +2,47 @@
 
 set -e
 
-gcc -Wall -Wextra -g mailclient.c -o mailclient
-gcc -Wall -Wextra -g popserver.c -o popserver
-gcc -Wall -Wextra -g smtpmail.c -o smtpmail
+
+files=("mailclient" "popserver" "smtpmail")
+
+clean() {
+    for binary in ${files[@]}; do
+        rm -f $binary
+    done
+}
+
+remove_folders() {
+    awk '{print $1}' "userlogincred.txt" | xargs /bin/rm -rf
+}
+
+build() {
+    for binary in ${files[@]}; do
+        gcc -Wall -Wextra -g "$binary.c" -o "$binary"
+    done
+}
+
+demo() {
+    tmux new-session \; \
+        send-keys './smtpmail 2525' C-m \; \
+        split-window -h \; \
+        send-keys './popserver 1110' C-m \; \
+        split-window -h \; \
+        send-keys './mailclient 2525 1110' C-m \; \
+        select-layout even-horizontal
+}
 
 
-tmux new-session \; \
-    send-keys './smtpmail 2525' C-m \; \
-    split-window -h \; \
-    send-keys './popserver 1110' C-m \; \
-    split-window -h \; \
-    send-keys './mailclient 2525 1110' C-m \; \
-    select-layout even-horizontal
+# build
+# demo
 
 
+if [[ "$1" == "clean" ]]; then
+    echo "Removing unwanted files"
+    clean
+elif [[ "$1" == "cleanall" ]]; then
+    remove_folders
+    clean
+else
+    build
+    demo
+fi
